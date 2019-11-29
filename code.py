b5d4d5d504d468f9795ec0d28560ae0409b337f0
9rishabh8 <58312673+9rishabh8@users.noreply.github.com>
2019-11-30 02:20:18 +0530

    Add files via upload
    It is a basic Machine Learning (beginners level) project.
    That predicts the final grade of students by accepting attributes such as their initial grades , sleeping time,failures,absences.

--- code.py
+++ code.py
@@ -6,19 +6,31 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
 
 data=pd.read_csv('student-mat.csv',sep=';')
 data=data[['G1','G2','G3','studytime','failures','absences']]
 
 X=np.array(data.drop(columns='G3'))
 y=np.array(data['G3'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+'''BEST=0
+for _ in range(30):
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.1)
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.1)
+    obj=LinearRegression()
+    obj.fit(X_train,y_train)
+    acc=obj.score(X_test,y_test)
+    print(acc)
+    if acc>BEST:
+        best=acc
+        with open('studentmodel.pickle','wb') as f:
+            pickle.dump(obj,f)'''
 
-obj=LinearRegression()
-obj.fit(X_train,y_train)
-acc=obj.score(X_test,y_test)
-print(acc)
+pickle_inanyname=open('studentmodel.pickle','rb')
+obj=pickle.load(pickle_inanyname)
 
 print('coefficient is=',obj.coef_)
 print('intercept is=',obj.intercept_)
@@ -26,3 +38,10 @@ print('intercept is=',obj.intercept_)
 predictions=obj.predict(X_test)
 for i in range(len(predictions)):
     print(predictions[i],X_test[i],y_test[i])
+
+p='G1'
+style.use('ggplot')
+pyplot.scatter(data[p],data['G3'])
+pyplot.xlabel(p)
+pyplot.ylabel('Final Grade')
+pyplot.show()
